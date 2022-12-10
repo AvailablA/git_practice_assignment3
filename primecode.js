b00d0450a774c856ec9ceacc77ce555902182eea
unknown <nikhilpillare98@gmail.com>
2022-12-10 20:49:46 +0530

    code-fix

--- primecode.js
+++ primecode.js
@@ -1,11 +1,14 @@
-function identifyPrime(num) {
+let num=13; 
+
+{
     let count = 0;
-    for(let i=1; i<=num; i++){
+    for(let i=2; i<=num; i++){
         if(num % i == 0){
             count++;
         }
     }
-    if(count == 2){
+
+    if(count == 1){
         console.log("Yes");
     }else{
         console.log("No");
